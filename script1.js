f03c5ff6cf46fd1758f82cf193e3034dedee6639
StepanovMaksim <130572592+StepanovMaksim@users.noreply.github.com>
2024-01-20 11:49:51 +0300

    final

--- script1.js
+++ script1.js
@@ -1,14 +1,21 @@
 const nextBtn = document.querySelector('.next-btn');
 const prevBtn = document.querySelector('.prev-btn');
 const getVideo = document.getElementsByTagName('video');
+const textAdv = document.getElementsByClassName('description');
 const slides = document.querySelectorAll('.slide');
 const numberOfSlides = slides.length;
 
 let slideNumber = 0;
 
+const textCenter = 'Забудьте о привычных маршрутах и окунитесь в мир незабываемых приключений! Мы предлагаем вам путешествие вашей жизни - тур по самым живописным лесам нашей страны. Отправляйтесь с нами в увлекательное путешествие по густым сибирским лесам, насладитесь красотой многовековых лесов Алтая или раскройте тайны могучих лесов Карелии.';
+
+let i = 0,
+    str = [];
+
 nextBtn.onclick = () => {
     slides.forEach((slide) => {
         slide.classList.remove('active');
+        slide.classList.remove('active1')
     });
 
     slideNumber++;
@@ -17,12 +24,13 @@ nextBtn.onclick = () => {
         slideNumber = 0;
     }
     slides[slideNumber].classList.add('active');
-
+    clearInterval(anim);
 }
 
 prevBtn.onclick = () => {
     slides.forEach((slide) => {
         slide.classList.remove('active');
+        slide.classList.remove('active1');
     });
 
     slideNumber--;
@@ -34,12 +42,25 @@ prevBtn.onclick = () => {
     slides[slideNumber].classList.add('active');
 }
 
+function textPlay(text) {
+    i = 0;
+    str = [];
+    let anim = setInterval(function() {
+        str[i] = text[i]; 
+        if (i < text.length) {
+           textAdv[0].textContent = str.join('');
+            i++;        
+        } else clearInterval(anim);
+    }, 100);
+    
+}
 
 const playVideo = () => {
-    slides.forEach((slide) => {
-        slide.classList.remove('active');
-    });
-    getVideo[0].style.zIndex = 101;
-    console.log(getVideo[0].style.zIndex)
+    slides[slideNumber].classList.remove('active');
+    slides[slideNumber].classList.add('active1');
+    getVideo[0].classList.add('active');
+    textPlay(textCenter);
 };
+
+
 
